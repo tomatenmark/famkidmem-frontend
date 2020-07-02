316b1de19146301ee9d0f34011e224fe81dbb006
tomatenmark <mh88@mailbox.org>
2020-07-02 17:53:38 +0200

    do initial things

---
+++ res/js/app.js
@@ -0,0 +1,16 @@
+Vue.config.devtools = config.vueDevToolsEnabled;
+
+
+const data = {
+    menuOpen: false
+};
+
+const methods = {
+
+};
+
+const app = new Vue({
+    el: '#app',
+    data: data,
+    methods: methods
+});
---
+++ res/js/config.js
@@ -0,0 +1,11 @@
+const config = {
+    //base url for the backend api
+    apiBase: '/api',
+
+    //enable (true) or disable (false) devTools for Vue.js in Browser-DevTools - false is recommended for production
+    vueDevToolsEnabled: true
+};
+
+
+
+
